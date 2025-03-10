6512291c7a55efef13144a2b2c1c3e88e8dbf690
Jlin-team <jasperlin410@gmail.com>
2025-03-10 14:12:22 +0800

    hw

--- student_assignment.py
+++ student_assignment.py
@@ -11,13 +11,21 @@ gpt_emb_version = 'text-embedding-ada-002'
 gpt_emb_config = get_model_configuration(gpt_emb_version)
 
 dbpath = "./"
-
+openai_ef = embedding_functions.OpenAIEmbeddingFunction(
+        api_key = gpt_emb_config['api_key'],
+        api_base = gpt_emb_config['api_base'],
+        api_type = gpt_emb_config['openai_type'],
+        api_version = gpt_emb_config['api_version'],
+        deployment_id = gpt_emb_config['deployment_name']
+    )
 def generate_hw01():
+    
     settings = Settings(persist_directory="chroma.sqlite3")
-    client = chromadb.Client(settings)
+    client = chromadb.PersistentClient(path=dbpath, settings=settings)
     collection = client.get_or_create_collection(
         name="TRAVEL",
-        metadata={"hnsw:space":"cosine"}
+        metadata={"hnsw:space":"cosine"},
+        embedding_function=openai_ef
     )
     df=pd.read_csv("COA_OpenData.csv")
     
@@ -54,10 +62,42 @@ def generate_hw01():
     )
     
     return collection
+
         
     
-def generate_hw02(question, city, store_type, start_date, end_date):
-    pass
+def generate_hw02(question:str, city:list, store_type:list, start_date:datetime, end_date:datetime):
+
+    client = chromadb.PersistentClient(path=dbpath)
+    collection = client.get_collection(name="TRAVEL", embedding_function=openai_ef)
+    start_ts=int(start_date.timestamp())
+    end_ts=int(end_date.timestamp())
+    
+    where_filter={
+        "$and":[
+        {"city":{"$in":city}},
+        {"type":{"$in":store_type}},
+        {"date":{"$gte":start_ts}},
+        {"date":{"$lte":end_ts}}
+        ]
+    }
+    
+    query_results = collection.query(
+        query_texts=question,
+        n_results=10,
+        where=where_filter
+    )
+    
+    distances = query_results.get("distances",[[]])[0]
+    metadatas = query_results.get("metadatas",[[]])[0]
+    
+    filtered=[]
+    for score, meta in zip(distances, metadatas):
+        if score >= 0.80:
+            filtered.append((score,meta["name"]))
+            
+    filtered = sorted(filtered, key=lambda x:x[0], reverse=True)
+    store_name = [name for score, name in filtered]
+    return store_name
     
 def generate_hw03(question, store_name, new_store_name, city, store_type):
     pass
