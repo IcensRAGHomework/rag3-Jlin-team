4aa13d68ea4e95bc16ad8a9945917a283a0486b4
Jlin-team <jasperlin410@gmail.com>
2025-03-11 11:16:25 +0800

    hw

--- student_assignment.py
+++ student_assignment.py
@@ -98,7 +98,54 @@ def generate_hw02(question, city, store_type, start_date, end_date):
     return store_names
     
 def generate_hw03(question, store_name, new_store_name, city, store_type):
-    pass
+    collection = generate_hw01()
+    
+    query_results = collection.query(
+        query_texts = [question],
+        n_results = 10,
+        where={"name": {"$eq": store_name}},
+        include=["metadatas"]
+    )
+    
+    if query_results["ids"]:
+        store_id=query_results["ids"][0][0]
+        metadata = query_results["metadatas"][0][0]
+        metadata["new_store_name"] = new_store_name
+        collection.update(ids=[store_id], metadatas=[metadata])
+        print(f"Update {store_name} with new_store_name: {new_store_name}")
+    else:
+        print(f"Store {store_name} not found.")
+    
+    where_filer = {
+        "$and":[
+            {"city":{"$in":city}},
+            {"type":{"$in":store_type}}
+        ]
+    }
+    
+    results = collection.query(
+        query_texts = [question],
+        n_results = 10,
+        where =where_filer,
+        include=["metadatas","distances"]
+    )
+    
+    if results.get("metadatas") and results.get("distances"):
+        filter_stores = []
+        for i, distance in enumerate(results["distances"][0]):
+            similarity = 1 - distance
+            if similarity >= 0.80:
+                metadata = results["metadatas"][0][i]
+                store_name = metadata.get("new_store_name", metadata["name"])
+                filter_stores.append((store_name, similarity))
+            
+        # sort by similarity to desc order
+        filter_stores.sort(key=lambda x:x[1], reverse=True)
+        store_names = [name for name,_ in filter_stores]
+        return store_names
+    
+    print("No store found.")
+    return []
     
 def demo(question):
     chroma_client = chromadb.PersistentClient(path=dbpath)
@@ -116,3 +163,14 @@ def demo(question):
     )
     
     return collection
+
+
+
+
+print(generate_hw03(
+    question="我想要找南投縣的田媽媽餐廳，招牌是蕎麥麵",
+    store_name="耄饕客棧",
+    new_store_name="田媽媽（耄饕客棧）",
+    city=["南投縣"],
+    store_type=["美食"]
+))
